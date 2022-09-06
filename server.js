318d71dc6983d636965e7af38ed5bb6828fad931
tjlinch <Tyler@Tylers-MacBook-Pro-8.local>
2022-09-05 20:13:55 -0600

    Post Route added to create new notes

--- server.js
+++ server.js
@@ -19,7 +19,34 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 //Get Route for notes page
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
+//Get Route for db.json
+app.get('/api/notes', (req, res) => res.json(notes));
 
+//Post Route for new notes
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+
+    let maxId = 99;
+
+    for (const note of notes) {
+        if (note.id > maxId) {
+            maxId = node.id;
+        };
+    };
+
+    newNote.id = maxId++;
+    notes.push(newNote);
+
+    //rewrite db.json with the new note
+    fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes), function (err) {
+        if (err) {
+            return console.info(err);
+        }
+        console.info('New note saved');
+    });
+
+    res.json(newNote);
+});
 
 app.listen(PORT, () => {
     console.info(`App listening on port ${PORT}`);
